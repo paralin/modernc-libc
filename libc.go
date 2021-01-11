643c46514b90c283ac49a6cd6bb8da5686090da0
jaap aarts <jaap.aarts1@gmail.com>
2021-01-11 21:54:37 +0100

    fix allignment

--- libc.go
+++ libc.go
@@ -712,6 +712,9 @@ func Xmemset(t *TLS, s uintptr, c int32, n types.Size_t) uintptr {
 		//this will make sure that on platforms where they are not equally alligned
 		//we clear out the first few bytes until allignment
 		bytesBeforeAllignment := s % unsafe.Alignof(uint64(0))
+		if bytesBeforeAllignment > uintptr(n) {
+			bytesBeforeAllignment = uintptr(n)
+		}
 		b := (*RawMem)(unsafe.Pointer(s))[:bytesBeforeAllignment:bytesBeforeAllignment]
 		n -= types.Size_t(bytesBeforeAllignment)
 		for i := range b {
@@ -719,12 +722,12 @@ func Xmemset(t *TLS, s uintptr, c int32, n types.Size_t) uintptr {
 		}
 
 		i64 := uint64(byte(c) + byte(c)<<8 + byte(c)<<16 + byte(c)<<24 + byte(c)<<32 + byte(c)<<40 + byte(c)<<48 + byte(c)<<56)
-		b8 := (*RawMem64)(unsafe.Pointer(s))[: n/8 : n/8]
+		b8 := (*RawMem64)(unsafe.Pointer(s + bytesBeforeAllignment))[: n/8 : n/8]
 		for i := range b8 {
 			b8[i] = i64
 		}
 
-		b = (*RawMem)(unsafe.Pointer(s))[: n%8 : n%8]
+		b = (*RawMem)(unsafe.Pointer(s + bytesBeforeAllignment + uintptr(n-n%8)))[: n%8 : n%8]
 		for i := range b {
 			b[i] = byte(c)
 		}
